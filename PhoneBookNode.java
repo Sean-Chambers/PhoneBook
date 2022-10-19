da04ceb410db212110c928961cb1e29f7f441127
Sarah <sarahhoekema20@gmail.com>
2022-10-19 12:21:47 -0700

    edited node

--- PhoneBookNode.java
+++ PhoneBookNode.java
@@ -1,5 +1,5 @@
 public class PhoneBookNode {
-  PhoneBookEntry entry;
+  private PhoneBookEntry entry;
   PhoneBookNode next;
 
   public PhoneBookNode(String firstName, String lastName, String address, String city,
@@ -20,5 +20,53 @@ public class PhoneBookNode {
 
   public String toString(){
     return entry.toString();
+  }
+
+  public String getFirstName(){
+    return entry.firstName;
+  }
+
+  public String getLastName(){
+    return entry.lastName;
+  }
+
+  public String getAddress(){
+    return entry.address;
+  }
+
+  public String getCity(){
+    return entry.city;
+  }
+
+  public int getZipcode(){
+    return entry.zipcode;
+  }
+
+  public String getPhoneNumber(){
+    return entry.phoneNumber;
+  }
+
+  public void setFirstName(String firstName){
+    entry.firstName = firstName;
+  }
+
+  public void setLastName(String lastName){
+    entry.lastName = lastName;
+  }
+
+  public void setAddress(String address){
+    entry.address = address;
+  }
+
+  public void setCity(String city){
+    entry.city = city;
+  }
+
+  public void setZipcode(int zipcode){
+    entry.zipcode = zipcode;
+  }
+
+  public void setPhoneNumber(String phoneNumber){
+    entry.phoneNumber = phoneNumber;
   }
 }
