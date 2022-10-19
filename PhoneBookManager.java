76d8fe74762e749994832c17708932b14497e765
Sarah <sarahhoekema20@gmail.com>
2022-10-19 13:26:38 -0700

    debugging add method

--- PhoneBookManager.java
+++ PhoneBookManager.java
@@ -15,15 +15,17 @@ public class PhoneBookManager {
     PhoneBookNode current = head;  
     if(head == null){
       head = new PhoneBookNode(lastName, firstName, address, city, zipcode, phoneNumber);
-    } else if(last.compareTo(current.entry.lastName) < 0 ||
-      (last.compareTo(current.entry.lastName) == 0 && first.compareTo(current.entry.firstName) < 0)){
+    } else if(last.compareTo(current.getLastName()) < 0 ||
+      (last.compareTo(current.getLastName()) == 0 && first.compareTo(current.getFirstName()) < 0)){
       head = new PhoneBookNode(lastName, firstName, address, city, zipcode, phoneNumber, head);
-    } else{ //TODO: debug sorting and adding process
+    } else { //TODO: debug sorting and adding process
       for(int i = 0; i < size; i++){
-        if(last.compareTo(current.next.entry.lastName) <= 0 && first.compareTo(current.next.entry.firstName) < 0){
+        if(current.next != null &&
+        last.compareTo(current.next.getLastName()) <= 0 &&
+        first.compareTo(current.next.getFirstName()) < 0){
           current.next = new PhoneBookNode(lastName, firstName, address, city,
           zipcode, phoneNumber, current.next);
-        } else{
+        } else {
           current = current.next;
         }
       }
@@ -31,23 +33,21 @@ public class PhoneBookManager {
     size++;
   }
 
-  public void modify(){
-    //TODO: complete modify method
-  }
+  //TODO: complete modify method
 
   public void delete(String lastName, String firstName, String address){
     PhoneBookNode current = head;
     for(int i = 0; i < size - 1; i++){ //TODO: debug delete method
-      if(current.next.entry.lastName.equals(lastName) &&
-      current.next.entry.firstName.equals(firstName) &&
-      current.next.entry.address.equals(address) && 
+      if(current.next.getLastName().equals(lastName) &&
+      current.next.getFirstName().equals(firstName) &&
+      current.next.getAddress().equals(address) && 
       current.next.next != null){
         current.next = current.next.next;
         size--;
         break;
-      } else if(current.next.entry.lastName.equals(lastName) &&
-      current.next.entry.firstName.equals(firstName) &&
-      current.next.entry.address.equals(address) && 
+      } else if(current.next.getLastName().equals(lastName) &&
+      current.next.getFirstName().equals(firstName) &&
+      current.next.getAddress().equals(address) && 
       current.next.next == null){
         current.next = null;
         size--;
@@ -60,7 +60,7 @@ public class PhoneBookManager {
   public void search(String lastName, String firstName){
     PhoneBookNode current = head;
     for(int i = 0; i < size; i++){
-      if(current.entry.lastName.equals(lastName) && current.entry.firstName.equals(firstName)){
+      if(current.getLastName().equals(lastName) && current.getFirstName().equals(firstName)){
         System.out.println(current);
       }
       current = current.next;
@@ -70,7 +70,7 @@ public class PhoneBookManager {
   public void search(String lastName){
     PhoneBookNode current = head;
     for(int i = 0; i < size; i++){
-      if(current.entry.lastName.equals(lastName)){
+      if(current.getLastName().equals(lastName)){
         System.out.println(current);
       }
       current = current.next;
@@ -86,13 +86,9 @@ public class PhoneBookManager {
     return current;
   }
 
-  public void move(int given, int target){
-    //TODO: complete move method
-  }
+  //TODO: complete move method
 
-  public PhoneBookNode movePhoneBook(String lastName, String firstName){
-    //TODO: complete movePhoneBook method
-  }
+  //TODO: complete movePhoneBook method
 
   public void print(){
     PhoneBookNode current = head;
--- PhoneBookTestClass.java
+++ PhoneBookTestClass.java
@@ -1,26 +1,11 @@
 public class PhoneBookTestClass {
   public static void main(String[] args){
-    String last1 = "Hoekema";
-    String last2 = "Hoekemo";
-    System.out.println(last1.compareTo(last2));
     PhoneBookManager phonebook = new PhoneBookManager();
 
     phonebook.add("Sarah", "Hoekemo", "4 Privet Drive", "Little Whinging", 12345, "3601234567"); 
     System.out.println(phonebook.get(0));
-    System.out.println(last1.compareTo(phonebook.get(0).entry.lastName));
     phonebook.add("Sarah", "Hoekema", "4 Privet Drive", "Little Whinging", 12345, "3601234567");
-    System.out.println(phonebook.get(0));
+    System.out.println(phonebook.get(1));
     phonebook.add("Serah", "Hoekema", "4 Privet Drive", "Little Whinging", 12345, "3601234567");
-    //System.out.println(phonebook.get(2));
-    phonebook.print();
-    System.out.println("---------------------");
-    phonebook.print();
-    System.out.println("---------------------");
-    phonebook.search("Hoekema");
-    System.out.println("---------------------");
-    phonebook.delete("Sarah", "Hoekema", "4 Privet Drive");
-    System.out.println(phonebook.size());
-    phonebook.delete("Serah", "Hoekema", "4 Privet Drive");
-    phonebook.print();
   }
 }
